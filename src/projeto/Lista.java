527bdbd14245464cc5c54bb100beca3083bc2a36
Bianca <BiEsteves2006@gmail.com>
2026-05-27 22:45:11 -0300

    adicionado o usuario a escolha de sair do programa

--- src/projeto/Lista.java
+++ src/projeto/Lista.java
@@ -4,51 +4,81 @@ import java.util.Scanner;
 
 public class Lista {
 
+    //PASSO1-MENU.PRINCIPAL
 	public static void main(String[] args) {
-		Scanner teclado = new Scanner(System.in);
-		int selecionar, tamanhoLista;
-		
-		System.out.println("=====================================");
-		System.out.println("-----------LISTA DE TAREFAS----------");
-		System.out.println("(1): ADIONAR TAREFA ");
-		System.out.println("(2): REMOVER TAREFA");
-		System.out.println("(3): MODIFICAR");
-		System.out.println("(4): SAIR");
-		System.out.println("=====================================");
-		System.out.println("Digite um núemro para selecionar: ");
-		System.out.println("=====================================");
-		selecionar = teclado.nextInt();
-	
-	System.out.println("=====================================");
-	System.out.println("Digite o tamanho a lista a ser criada: ");
-	System.out.println("=====================================");
-	tamanhoLista = teclado.nextInt();
-	adicionar(selecionar, tamanhoLista);
-	
-	}
 
-	public static void adicionar(int selecionar, int tamanhoLista) { //Passo2
-		Scanner teclado = new Scanner(System.in);
-		   System.out.println("=====================================");
-		   System.out.println("-----------LISTA DE TAREFAS----------");
-		   System.out.println("=====================================");
+        Scanner teclado = new Scanner(System.in);
 
-		   String[] lista = new String[tamanhoLista];
-		   
-		   System.out.println("Digite a sua tarefa: ");
-		
-		for(int i=0; i<tamanhoLista; i++) {
-			
-			lista[i] = teclado.nextLine();
-			
-			}
-		
-	        for(int i=0; i<tamanhoLista; i++) {
-				
-	        	System.out.println(lista[i]);
-		
-	}
-	        
-	}
+        int selecionar = 0;
+        int tamanhoLista;
+
+        System.out.println("=====================================");
+        System.out.println("Digite o tamanho da lista:");
+        System.out.println("=====================================");
+
+        tamanhoLista = teclado.nextInt();
+
+        //mantem.o.programa.funcionando
+        while (selecionar != 4) {
+
+            System.out.println("=====================================");
+            System.out.println("-----------LISTA DE TAREFAS----------");
+            System.out.println("(1): ADICIONAR TAREFA ");
+            System.out.println("(2): REMOVER TAREFA");
+            System.out.println("(3): MODIFICAR");
+            System.out.println("(4): SAIR");
+            System.out.println("=====================================");
+            System.out.println("Digite um número para selecionar:");
+            System.out.println("=====================================");
+
+            selecionar = teclado.nextInt();
+
+            if (selecionar == 1) {
+
+                adicionar(selecionar, tamanhoLista);
+
+            }
+
+            //Enecerra.o.programa
+            if (selecionar == 4) {
+
+                System.out.println("Programa encerrado!");
+
+            }
+
+        }
+
+    }
+
+    //PASSO1-ADICIONAR.TAREFA
+	public static void adicionar(int selecionar, int tamanhoLista) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("=====================================");
+        System.out.println("-----------LISTA DE TAREFAS----------");
+        System.out.println("=====================================");
+
+        String[] lista = new String[tamanhoLista];
+
+        teclado.nextLine();
+
+        System.out.println("Digite a sua tarefa:");
+
+        //Adicionar.as.tarefas
+        for (int i = 0; i < tamanhoLista; i++) {
+
+            lista[i] = teclado.nextLine();
+
+        }
+
+        //Mostrar.as.tarefas
+        for (int i = 0; i < tamanhoLista; i++) {
+
+            System.out.println(lista[i]);
+
+        }
+
+    }
+
 }
-
